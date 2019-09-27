26a6ad66447c06a244423d88411ebce2118036c4
Kevin Steele <kevdsteele@gmail.com>
2019-09-27 14:56:36 -0400

    change uri

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect('mongodb://greenpoint:Savetheworld19@ds051595.mlab.com:51595/heroku_m0nlvt7s' );
+mongoose.connect(process.env.MONGODB_URI );
 
 // Start the API server
 app.listen(PORT, function() {
